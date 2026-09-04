af51d5c4544fcc8b96f81eefff63ce9435ce9e45
Güneş Oktay Özdoğan <gunes.ozdogan95@gmail.com>
2026-09-04 21:14:24 +0300

    Add: binary-search - submission-10

---
+++ Data Structures & Algorithms/binary-search/submission-10.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let start = 0;
+        let end = nums.length - 1;
+
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2);
+
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+
+            if (nums[mid] === target) {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
